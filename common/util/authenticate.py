e289eec52c8a4aac1c6c7bba1865fb8628dcebb2
Suraj S <suraj.s@cognizant.com>
2018-02-14 22:06:51 +0530

    first commit

---
+++ common/util/authenticate.py
@@ -0,0 +1,30 @@
+
+__all__ = [
+    'login',
+    'register'
+]
+
+
+def login ( username, password):
+    """
+    IMPLEMENTATION LOGIC - FETCH FROM DB 
+    THE HASH AND USERNAME TO VALIDATE THE
+    WORKING
+    """
+    result = 'True'
+    print "The function is working:login"
+    print "The username is"
+    print username
+    print "The password is"
+    print password
+    return bool(result)
+
+def register (username, email, phone):
+
+    """
+    IMPLEMENTATION LOGIC - INSERT INTO DB
+    RELEVANT VALUES
+
+    """
+    print "The function is working:register"
+
